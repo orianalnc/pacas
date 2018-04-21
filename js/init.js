5e11451e9640c1a07de5390d8d2e0691cbbe8c40
Orianalnc <orianalnc94@gmail.com>
2018-04-21 13:47:40 -0430

    Pacas sin Video

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('select').material_select();
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
